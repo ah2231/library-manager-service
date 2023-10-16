b95e263ea070065438cbd3269d3acc374bdeee77
a <a@example.com>
2023-10-16 22:37:49 +0100

    Initial commit

---
+++ src/main/java/com/librarymanagerservice/LibraryManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.librarymanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerServiceApplication.class, args);
+    }
+
+}
